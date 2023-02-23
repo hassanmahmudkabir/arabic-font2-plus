933323081c6583d72b57b2b4a8bfd30739a9b645
almahmudbd <almahmudbd@yahoo.com>
2023-02-24 03:11:48 +0600

    Update arabic-font2-plus.php

--- arabic-font2-plus.php
+++ arabic-font2-plus.php
@@ -9,9 +9,10 @@ Author URI: https://thealmahmud.blogspot.com/
 */
 
 function custom_arabic_font_shortcode($atts, $content = null) {
-  // Get font family and font size from shortcode attributes, or use the default font and font size if not specified
+  // Get font family, Line gap and font size from shortcode attributes, or use the default font and font size if not specified
   $font_family = isset($atts['font']) ? $atts['font'] : 'noorehira';
-  $font_size = isset($atts['size']) ? $atts['size'] : '22px';
+  $font_size = isset($atts['size']) ? $atts['size'] : '26px';
+  $line_height = isset($atts['gap']) ? $atts['size'] : '46px';
 
   // Check if the font file exists
   $font_file = plugin_dir_path(__FILE__) . 'fonts/' . $font_family . '.ttf';
@@ -24,7 +25,7 @@ function custom_arabic_font_shortcode($atts, $content = null) {
   wp_enqueue_style('custom-arabic-font-' . $font_family, plugin_dir_url(__FILE__) . 'fonts/' . $font_family . '.css');
 
   // Return the content with the custom font and font size applied
-  return '<span style="font-family: ' . $font_family . '; font-size: ' . $font_size . ';">' . $content . '</span>';
+  return '<span style="font-family: ' . $font_family . '; font-size: ' . $font_size . '; line-height: ' . $line_height . ';">' . $content . '</span>';
 }
 
 add_shortcode('arabic', 'custom_arabic_font_shortcode');
